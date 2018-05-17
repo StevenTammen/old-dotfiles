25f6f66a523d7fd3e6180a9ad70e4f5a3323ec13
Steven Tammen <steven@steventammen.com>
2018-05-16 20:26:04 -0400

    Remove local/ from repo. Don't symlink bin/. Update conf files to reflect this

--- config/fish/conf.d/omf.fish
+++ config/fish/conf.d/omf.fish
@@ -5,18 +5,3 @@ set -q XDG_DATA_HOME
 
 # Load Oh My Fish configuration.
 source $OMF_PATH/init.fish
-
-# Add .local/bin to PATH
-set PATH $PATH ~/.local/bin
-
-# Pretty colors. Toggle between day/night with sh scripts in .local/bin
-day
-eval (dircolors -c ~/dotfiles/dir_colors | sed 's/>&\/dev\/null$//')
-
-# Aliases
-alias dbox "cd /mnt/c/Users/steve/Dropbox"
-alias stc "cd /mnt/c/Users/steve/Dropbox/steventammen.com"
-alias chc "cd /mnt/c/Users/steve/Dropbox/chrya.com"
-alias dboxr "cd /mnt/c/Users/steve/Dropbox; ranger"
-alias stcr "cd /mnt/c/Users/steve/Dropbox/steventammen.com; ranger"
-alias chcr "cd /mnt/c/Users/steve/Dropbox/chrya.com; ranger"
---
+++ config/omf/init.fish
@@ -0,0 +1,14 @@
+# Add dotfiles/bin to PATH
+set PATH $PATH ~/dotfiles/bin
+
+# Pretty colors. Toggle between day/night with sh scripts in dotfiles/bin
+day
+eval (dircolors -c ~/dotfiles/dir_colors | sed 's/>&\/dev\/null$//')
+
+# Aliases
+alias dbox "cd /mnt/c/Users/steve/Dropbox"
+alias stc "cd /mnt/c/Users/steve/Dropbox/steventammen.com"
+alias chc "cd /mnt/c/Users/steve/Dropbox/chrya.com"
+alias dboxr "cd /mnt/c/Users/steve/Dropbox; ranger"
+alias stcr "cd /mnt/c/Users/steve/Dropbox/steventammen.com; ranger"
+alias chcr "cd /mnt/c/Users/steve/Dropbox/chrya.com; ranger"
