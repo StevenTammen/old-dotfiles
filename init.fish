4825806dbb28de5d492e04b582ff3768991c5d6e
Steven Tammen <steven@steventammen.com>
2018-07-17 12:08:11 -0400

    Add alias for p2.

--- init.fish
+++ init.fish
@@ -16,3 +16,6 @@ alias chc "cd /mnt/c/Users/steve/Dropbox/chrya.com"
 alias dboxr "cd /mnt/c/Users/steve/Dropbox; ranger"
 alias stcr "cd /mnt/c/Users/steve/Dropbox/steventammen.com; ranger"
 alias chcr "cd /mnt/c/Users/steve/Dropbox/chrya.com; ranger"
+
+alias p1 "rsync -a --delete /mnt/c/Users/steve/Desktop/Projects/Tammen-Steven-p1/ tammen@nike.cs.uga.edu:/home/ugrads/tammen/Tammen-Steven-p1"
+alias p2 "rsync -a --delete /mnt/c/Users/steve/Desktop/Projects/Tammen-Steven-p2/ tammen@nike.cs.uga.edu:/home/ugrads/tammen/Tammen-Steven-p2"
