1ae74b9dec3f207be9c464c939d3478fcb03a6f1
Steven Tammen <steven@steventammen.com>
2018-08-11 14:50:41 -0400

    Add conveniences for Hugo use. Update aliases.

--- init.fish
+++ init.fish
@@ -9,15 +9,29 @@ eval (dircolors -c ~/dotfiles/dir_colors | sed 's/>&\/dev\/null$//')
 set VISUAL vim
 set EDITOR $VISUAL
 
-# Aliases
+# Movement aliases
 alias dbox "cd /mnt/c/Users/steve/Dropbox"
-alias stc "cd /mnt/c/Users/steve/Dropbox/steventammen.com"
-alias chc "cd /mnt/c/Users/steve/Dropbox/chrya.com"
 alias dboxr "cd /mnt/c/Users/steve/Dropbox; ranger"
-alias stcr "cd /mnt/c/Users/steve/Dropbox/steventammen.com; ranger"
-alias chcr "cd /mnt/c/Users/steve/Dropbox/chrya.com; ranger"
 
+alias stc "cd /mnt/c/Users/steve/Desktop/Projects/steventammen.com"
+alias stcr "cd /mnt/c/Users/steve/Desktop/Projects/steventammen.com; ranger"
+
+alias chc "cd /mnt/c/Users/steve/Desktop/Projects/chrya.com"
+alias chcr "cd /mnt/c/Users/steve/Desktop/Projects/chrya.com; ranger"
+
+alias hiem "cd /mnt/c/Users/steve/Desktop/Projects/hiem"
+alias hiemr "cd /mnt/c/Users/steve/Desktop/Projects/hieam; ranger"
+
+alias ull "cd /mnt/c/Users/steve/Desktop/Projects/unicode-language-layers"
+alias ullr "cd /mnt/c/Users/steve/Desktop/Projects/unicode-language-layers; ranger"
+
+# Rsync aliases
 alias p1 "rsync -a --delete /mnt/c/Users/steve/Desktop/Projects/Tammen-Steven-p1/ tammen@nike.cs.uga.edu:/home/ugrads/tammen/Tammen-Steven-p1"
 alias p2 "rsync -a --delete /mnt/c/Users/steve/Desktop/Projects/Tammen-Steven-p2/ tammen@nike.cs.uga.edu:/home/ugrads/tammen/Tammen-Steven-p2"
 alias p3 "rsync -a --delete /mnt/c/Users/steve/Desktop/Projects/Tammen-Steven-p3/ tammen@nike.cs.uga.edu:/home/ugrads/tammen/Tammen-Steven-p3"
 alias final-p "rsync -a --delete /mnt/c/Users/steve/Desktop/Projects/Tammen-Steven-final/ tammen@nike.cs.uga.edu:/home/ugrads/tammen/Tammen-Steven-final"
+
+# Hugo aliases
+
+alias sup "hugo.exe server -D --navigateToChanged" # "start up server"
+
